87427f7527d2da90914384e1b95a125f18a9a013
MuhammadOmarAzhar <143085528+MuhammadOmarAzhar@users.noreply.github.com>
2023-11-10 21:26:36 +0500

    Toaster

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,13 @@
 import '@/styles/globals.css';
 import {QueryClient, QueryClientProvider} from 'react-query';
+import {ToastContainer} from 'react-toastify';
 
 const queryClient = new QueryClient();
 
 export default function App({Component, pageProps}) {
   return (
     <QueryClientProvider client={queryClient}>
+      <ToastContainer />
       <Component {...pageProps} />;
     </QueryClientProvider>
   );
