e71b87837b757913611919234083183a5a2eaf0f
Rohail Abbas <rohail.123452@gmail.com>
2023-11-13 18:13:46 +0500

    changes student details

--- src/pages/student-detail/index.jsx
+++ src/pages/student-detail/index.jsx
@@ -57,108 +57,120 @@ const studentDetail = () => {
       <div className='flex justify-center'>
         <div className='bg-white p-6 rounded-lg h-fit w-fit'>
           <form className='w-full max-w-lg'>
-            <div className='flex items-center p-6'>
+            <div className='flex justify-center  p-6'>
               <div className='text-black text-5xl'>
-                <UserOutlined />
+                <UserOutlined style={{color: '#9CA3AF', marginRight: 5}} />
               </div>
-              <div>
-                <h1 className='text-black text-lg font-bold uppercase'>
+              <div className=' grid items-end'>
+                <h1 className='text-gray-400 text-md font-semibold uppercase'>
                   {studentData.firstName} {studentData.lastName}
                 </h1>
               </div>
             </div>
-            <Divider>Personal Information</Divider>
+            <Divider style={{color: 'lightgray'}}>Personal Information</Divider>
             <div className='flex flex-wrap -mx-3 mb-6'>
               <div className='w-full md:w-1/2 px-3 mb-6 md:mb-0'>
-                <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>
+                <label className='block uppercase tracking-wide text-gray-800 text-sm font-bold '>
                   First Name
                 </label>
-                <p className='text-gray-700'>{studentData.firstName}</p>
+                <p className='text-gray-400 text-sm'>{studentData.firstName}</p>
               </div>
               <div className='w-full md:w-1/2 px-3'>
-                <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>
+                <label className='block uppercase tracking-wide text-gray-800 text-sm font-bold'>
                   Last Name
                 </label>
-                <p className='text-gray-700'>{studentData.lastName}</p>
+                <p className='text-gray-400 text-sm'>{studentData.lastName}</p>
               </div>
               <div className='w-full mt-6 md:w-1/2 px-3'>
-                <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>
+                <label className='block uppercase tracking-wide text-gray-800 text-sm font-bold'>
                   Father/Guardian Name
                 </label>
-                <p className='text-gray-700'>{studentData.fatherName}</p>
+                <p className='text-gray-400 text-sm'>
+                  {studentData.fatherName}
+                </p>
               </div>
               <div className='w-full mt-6 md:w-1/2 px-3'>
-                <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>
+                <label className='block uppercase tracking-wide text-gray-800 text-sm font-bold'>
                   Email
                 </label>
-                <p className='text-gray-700'>{studentData.email}</p>
+                <p className='text-gray-400 text-sm'>{studentData.email}</p>
               </div>
             </div>
             <div className='flex flex-wrap -mx-3 mb-6'>
               <div className='w-full px-3 md:w-1/2'>
-                <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>
+                <label className='block uppercase tracking-wide text-gray-800 text-sm font-bold'>
                   Contact
                 </label>
-                <p className='text-gray-700'>{studentData.phone}</p>
+                <p className='text-gray-400 text-sm'>{studentData.phone}</p>
               </div>
               <div className='w-full px-3 md:w-1/2'>
-                <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>
+                <label className='block uppercase tracking-wide text-gray-800 text-sm font-bold'>
                   Class
                 </label>
-                <p className='text-gray-700'>{studentData.studentClass}</p>
+                <p className='text-gray-400 text-sm'>
+                  {studentData.studentClass}
+                </p>
               </div>
             </div>
 
             <div className='flex flex-wrap -mx-3 mb-2'>
-              <div className='w-full md:w-1/3 px-3 mb-6'>
-                <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>
-                  City
-                </label>
-                <p className='text-gray-700'>{studentData.address}</p>
-              </div>
               <div className='w-full md:w-1/3 px-3 mb-6 md:mb-0'>
-                <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>
-                  Blood group
-                </label>
-                <p className='text-gray-700'>{studentData.bloodgroup}</p>
-              </div>
-              <div className='w-full md:w-1/3 px-3 mb-6 md:mb-0'>
-                <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>
-                  Gender
-                </label>
-                <p className='text-gray-700'>{studentData.gender}</p>
-              </div>
-              <div className='w-full md:w-1/3 px-3 mb-6 md:mb-0'>
-                <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>
+                <label className='block uppercase tracking-wide text-gray-800 text-sm font-bold'>
                   Religion
                 </label>
-                <p className='text-gray-700'>{studentData.religion}</p>
+                <p className='text-gray-400 text-sm'>{studentData.religion}</p>
               </div>
-              <Divider>Fee Structure</Divider>
+              <div className='w-full md:w-1/3 px-3 mb-6 md:mb-0'>
+                <label className='block uppercase tracking-wide text-gray-800 text-sm font-bold'>
+                  Blood group
+                </label>
+                <p className='text-gray-400 text-sm'>
+                  {studentData.bloodgroup}
+                </p>
+              </div>
+              <div className='w-full md:w-1/3 px-3 mb-6 md:mb-0'>
+                <label className='block uppercase tracking-wide text-gray-800 text-sm font-bold'>
+                  Gender
+                </label>
+                <p className='text-gray-400 text-sm'>{studentData.gender}</p>
+              </div>
+              <div className='w-full md:w-1/3 px-3 mt-6'>
+                <label className='block uppercase tracking-wide text-gray-800 text-sm font-bold'>
+                  City
+                </label>
+                <p className='text-gray-400 text-sm'>{studentData.address}</p>
+              </div>
+              <Divider style={{color: 'lightgray'}}>Fee Structure</Divider>
             </div>
             <div className='flex flex-wrap -mx-3 mb-6'>
               <div className='w-full md:w-1/2 px-3 mb-6 md:mb-0'>
-                <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>
+                <label className='block uppercase tracking-wide text-gray-800 text-sm font-bold'>
                   Admission fee
                 </label>
-                <p className='text-gray-700'>{studentData.admissionFee}</p>
+                <p className='text-gray-400 text-sm'>
+                  {studentData.admissionFee}
+                </p>
               </div>
               <div className='w-full md:w-1/2 px-3'>
-                <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>
+                <label className='block uppercase tracking-wide text-gray-800 text-sm font-bold'>
                   Tuition fee
                 </label>
-                <p className='text-gray-700'>{studentData.tuitionFee}</p>
+                <p className='text-gray-400 text-sm'>
+                  {studentData.tuitionFee}
+                </p>
               </div>
               <div className='w-full mt-6 md:w-1/2 px-3'>
-                <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>
+                <label className='block uppercase tracking-wide text-gray-800 text-sm font-bold'>
                   Transport fee
                 </label>
-                <p className='text-gray-700'>{studentData.transportFee}</p>
+                <p className='text-gray-400 text-sm'>
+                  {studentData.transportFee}
+                </p>
               </div>
               <Divider />
               <div className='w-full flex justify-between items-center mt-6 mx-2'>
                 <button
-                  className='bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline-gray'
+                  className='bg-gray-500 hover:bg-gray-700 text-white  py-1.5 px-2 rounded focus:outline-none focus:shadow-outline-gray'
                   type='button'
                   onClick={handleBack}
                 >
@@ -166,18 +178,18 @@ const studentDetail = () => {
                 </button>
                 <div>
                   <button
-                    className='bg-green-500 hover:bg-green-700 text-white font-bold m-2 py-2 px-4 rounded focus:outline-none focus:shadow-outline-green'
+                    className='bg-blue-500 hover:bg-blue-700 text-white m-2  py-1.5 px-2 rounded focus:outline-none focus:shadow-outline-blue'
+                    type='button'
+                    onClick={handleOpenModal}
+                  >
+                    New Fee Structure
+                  </button>
+                  <button
+                    className='bg-green-500 hover:bg-green-700 text-white  m-2 py-1.5 px-2 rounded focus:outline-none focus:shadow-outline-green'
                     type='button'
                     onClick={handleOpenModal}
                   >
                     Fee Collection
-                  </button>
-                  <button
-                    className='bg-blue-500 hover:bg-blue-700 text-white m-2 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline-blue'
-                    type='button'
-                    onClick={handleOpenModal}
-                  >
-                    Fee Structure
                   </button>
                 </div>
                 <Modal
